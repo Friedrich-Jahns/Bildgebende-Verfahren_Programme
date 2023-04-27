c872c718fc6f7573a29cd8b735a9960e5790d6e5
Friedrich-Jahns <jahnsfriedrich@gmail.com>
2023-04-27 13:29:12 +0200

    test

--- Test/Network_train.py
+++ Test/Network_train.py
@@ -9,5 +9,9 @@ class N:
 
 
 with open('Test/data.txt') as tf:
-    data = np.genfromtxt(tf,delimiter=' ')
-print(data)
+    data = np.genfromtxt(tf,delimiter='\t')
+
+
+data[1]
+for i in range(10):
+    for j in  
--- Test/data_gen.py
+++ Test/data_gen.py
@@ -6,8 +6,16 @@ dat = Path("Test/data.txt")
 dat.touch()
 nf = open(dat,"w")
 
-for i in np.arange(0,10):
-    nf.write(str(np.random.randint(2,size=3))+'\n')
 
+
+for i in np.arange(0,10):
+    for j in np.arange(3):
+        nf.write(str(np.random.randint(2)))
+        if j == 2:
+            break
+        else:
+            nf.write('\t')
+
+    nf.write('\n')
 
 nf.close()
