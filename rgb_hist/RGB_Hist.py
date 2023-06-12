6362e4966c7df98a8b51cee84e8e3f8edaf5453d
Friedrich-Jahns <jahnsfriedrich@gmail.com>
2023-06-12 19:47:13 +0200

    newfile

---
+++ rgb_hist/RGB_Hist.py
@@ -0,0 +1,15 @@
+__file__ = 'RGB_Hist.py'
+
+
+from matplotlib import pyplot as plt
+import numpy as np
+from PIL import Image
+import pathlib
+import os
+
+print(pathlib.Path(__file__).absolute())
+#for file in sorted(os.listdir(pathlib.Path(__file__).absolute().parent)):
+#    imgi = Image.open(f'{file}.png')
+#    print(imgi)
+
+
